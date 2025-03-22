7d7cfc285de95c9a90c574b999084baf9bbbd2d1
Evan Hallmark <ehallmar@usc.edu>
2025-03-21 19:15:05 -0700

    somewhat working

--- my_agent/alice.py
+++ my_agent/alice.py
@@ -28,38 +28,3 @@ workflow = create_workflow("alice", alice)
 # This compiles it into a LangChain Runnable,
 # meaning you can use it as you would any other runnable
 graph = workflow.compile()
-
-'''
-invoice = None
-
-# example with a multiple tool calls in succession
-for chunk in app.stream(
-    {"messages": [("human", "create an invoice with amount 100")]},
-    stream_mode="values",
-):
-    if chunk["messages"][-1].name == 'create_invoice':
-        invoice = chunk["messages"][-1].content
-        print(f'found payment request: {invoice}')
-    chunk["messages"][-1].pretty_print()
-
-if invoice:
-    invoice = json.loads(invoice)
-    
-    for chunk in app.stream(
-        {"messages": [("human", f"check the status of payment request {invoice['r_hash_str']}")]},
-        stream_mode="values",
-    ):
-        chunk["messages"][-1].pretty_print()
-
-    for chunk in app.stream(
-        {"messages": [("human", f"pay the invoice with payment request {invoice['payment_request']}")]},
-        stream_mode="values",
-    ):
-        chunk["messages"][-1].pretty_print()
-
-    for chunk in app.stream(
-        {"messages": [("human", f"check the status of payment request {invoice['r_hash_str']}")]},
-        stream_mode="values",
-    ):
-        chunk["messages"][-1].pretty_print()
-'''
